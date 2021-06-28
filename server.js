f7754979a28474e57ff7b63374136c78fbbd8f40
Shayne Krogfoss <skrogfoss@gmail.com>
2021-06-28 15:36:21 -0700

    Pass configured db

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 // Express framework and body parsing middleware
 const express = require('express');
 const bodyParser = require('body-parser');
-const db = require('node-json-db');
+const jsonDb = require('node-json-db');
+const { Config } = require('node-json-db/dist/lib/JsonDBConfig');
 
 // Set up express instance to handle JSON data
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const db = new jsonDb(new Config('./data/orpyx-orgs'));
 const routes = require('./routes/routes')(app, db);
 
 const server = app.listen(3000, () => {
