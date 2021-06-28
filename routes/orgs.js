4227360064dcec8f111d1b58c9c61acc8db80143
Shayne Krogfoss <skrogfoss@gmail.com>
2021-06-28 16:01:33 -0700

    Create read helper method

--- routes/orgs.js
+++ routes/orgs.js
@@ -1,11 +1,19 @@
 const orgRoutes = (app, db) => {
     const DATA_PATH = './data/orpyx-orgs.json'
 
-    app.get('/orgs', (req, res) => {
+    // Reader helper method
+    const readFile = (callback) => {
         db.readFile(DATA_PATH, 'utf8', (err, data) => {
-            res.send(data)
-        })
-    })
+            callback(data)
+        });
+    };
+
+    // Get all orgs
+    app.get('/orgs', (req, res) => {
+        readFile(data => {
+            res.send(data);
+        });
+    });
 }
 
 module.exports = orgRoutes;
