e3f6787dededc0926d0191344dab4684324bd026
Shayne Krogfoss <skrogfoss@gmail.com>
2021-06-28 15:45:14 -0700

    Use stock fs

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 // Express framework and body parsing middleware
 const express = require('express');
 const bodyParser = require('body-parser');
-const jsonDb = require('node-json-db');
-const { Config } = require('node-json-db/dist/lib/JsonDBConfig');
+const db = require('fs')
 
 // Set up express instance to handle JSON data
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const db = new jsonDb(new Config('./data/orpyx-orgs'));
 const routes = require('./routes/routes')(app, db);
 
 const server = app.listen(3000, () => {
